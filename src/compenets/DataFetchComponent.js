bc9f4f5b8a5f7a59becdc32ce2ee43244da4d032
khalidalfaifi <k.y.alfaifi@gmail.com>
2023-12-19 12:38:31 +0300

    modified:   src/compenets/DataFetchComponent.js

--- src/compenets/DataFetchComponent.js
+++ src/compenets/DataFetchComponent.js
@@ -1,79 +1,50 @@
 import React, { useState } from 'react';
-import { useApolloClient, gql } from '@apollo/client';
 
 function QueryForm({ onQuerySubmit }) {
-  const [timeRange, setTimeRange] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [dataType, setDataType] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onQuerySubmit({ timeRange, dataType });
+    onQuerySubmit({ startDate, endDate, dataType });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Time Range:
-        <input
-          type="text"
-          value={timeRange}
-          onChange={e => setTimeRange(e.target.value)}
-        />
-      </label>
-      <label>
-        Data Type:
-        <select value={dataType} onChange={e => setDataType(e.target.value)}>
-          <option value="HeartRate">Heart Rate</option>
-          <option value="NoiseLevel">Noise Level</option>
-          {/* Add other data types as needed */}
-        </select>
-      </label>
+      <div>
+        <label>
+          Start Date (YYYY-MM-DD):
+          <input
+            type="date"
+            value={startDate}
+            onChange={e => setStartDate(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          End Date (YYYY-MM-DD):
+          <input
+            type="date"
+            value={endDate}
+            onChange={e => setEndDate(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Data Type:
+          <select value={dataType} onChange={e => setDataType(e.target.value)}>
+            <option value="HeartRate">Heart Rate</option>
+            <option value="NoiseLevel">Noise Level</option>
+            {/* Add other data types as needed */}
+          </select>
+        </label>
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
 }
 
-function DataFetchComponent() {
-  const client = useApolloClient();
-  const [fetchedData, setFetchedData] = useState(null);
-
-  const handleQuerySubmit = async ({ timeRange, dataType }) => {
-    try {
-      const { data } = await client.query({
-        query: gql`
-          query GetAutismData($timeRange: String!, $dataType: String!) {
-            getAutismData(timeRange: $timeRange, dataType: $dataType) {
-              // Define the fields you want to fetch
-              // For example:
-              // _id
-              // UserId
-              // Timestamp
-              // HeartRate { BPM, Anomaly }
-              // NoiseLevel { Decibels, Anomaly }
-            }
-          }
-        `,
-        variables: { timeRange, dataType },
-      });
-      setFetchedData(data.getAutismData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      // Handle the error appropriately
-    }
-  };
-
-  return (
-    <div>
-      <QueryForm onQuerySubmit={handleQuerySubmit} />
-      {fetchedData && (
-        <div>
-          {/* Render your fetched data here */}
-          {/* This is an example, adjust it according to your data structure */}
-          <pre>{JSON.stringify(fetchedData, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default DataFetchComponent;
+export default QueryForm;
